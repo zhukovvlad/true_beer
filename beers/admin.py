a4ec32ab00aac8fd403f44f7c44686468e30ccc2
Vladimir Zhukov <zhukovvlad@yandex.ru>
2020-03-11 01:36:39 +0300

    Second commit

--- beers/admin.py
+++ beers/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Beer
+from .models import Beer, Brewery, Style, Hop
 
 admin.site.register(Beer)
+admin.site.register(Brewery)
+admin.site.register(Style)
+admin.site.register(Hop)
